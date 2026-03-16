57cd004a7cc83607b53754cb9234dfc73d8d22bd
whocarree7-ux <whocarree7@gmail.com>
2026-03-16 10:29:40 +0530

    Delete life_sim_bot_complete directory

--- life_sim_bot_complete/utils/embeds.py
+++
@@ -1,5 +0,0 @@
-
-import discord
-
-def simple_embed(title, description):
-    return discord.Embed(title=title, description=description)
--- life_sim_bot_complete/utils/randomizer.py
+++
@@ -1,5 +0,0 @@
-
-import random
-
-def chance(percent):
-    return random.random() < percent/100
